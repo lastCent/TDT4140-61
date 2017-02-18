393a9998128644c801df0ba20fa25db1ca7fdc3b
lastCent <kongekobra@msn.com>
2017-02-18 23:36:57 +0100

    Hotfix, models.py max_length issue

--- quiz/models.py
+++ quiz/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TestModel(models.Model):
     """ Models are the interface to the database """
-    name = models.CharField()
+    name = models.CharField(unique=True, max_length=20)
     number = models.IntegerField()
 
     def get_name(self):
