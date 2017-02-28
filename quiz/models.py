e591aee7c69c556cf35225d5b54afd43d3a0b7e6
michoy <michael.hoyer3@gmail.com>
2017-02-28 16:46:26 +0100

    init

--- quiz/models.py
+++ quiz/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=80)
     # todo: mail, tlf, passord
+    mail = models.EmailField()
+
 
     def __str__(self):
         return self.name
