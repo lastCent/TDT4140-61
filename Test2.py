2d3725b92849ebda0455f7b76382bd6f57f99e72
Andreas Bakke Moan <abakkem@gmail.com>
2017-03-07 20:29:54 +0100

    ALL HAIL PUTIN - THE LEGEND

---
+++ Test2.py
@@ -0,0 +1,12 @@
+from random import *
+
+def picknumbers():
+    WN=list[0,0,0,0,0]
+    EN=list[0,0,0]
+    x=1
+    while(x<6):
+        y=random.uniform(1,34)
+        x=x+1
+    while(x>5&x<8):
+        EN(x)=random.uniform(1,36)
+        x=x+1
