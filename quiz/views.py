513f6071793a73871e40235e89e15ae685b8231b
michoy <michael.hoyer3@gmail.com>
2017-03-08 13:39:00 +0100

    div fix on courses

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from quiz.forms import QuestionForm
-from quiz.models import Question, Course, CourseExercises, Exercise
+from quiz.models import Question, Course, CourseExercises, Exercise, CourseMembers
 
 
-# @permission_required() todo: find parameter name
+@permission_required('lecturer')
 def add_question(request):
     """ Loads template to get neccecary data for the question object, and saves it to database """
     added = False
@@ -20,6 +20,10 @@ def add_question(request):
 
     q_list = Question.objects.all()
     return render(request, 'add_question.html', {'form': form, 'added': added, 'questions': q_list})
+
+
+def base(request):
+    return render(request, 'base.html')
 
 
 @login_required
@@ -41,20 +45,23 @@ def view_question(request, exer_id):
         return render(request, 'view_question.html', context)
 
 
-def base(request):
-    return render(request, 'base.html')
-
-
-@login_required
-def courses_page(request):  # todo: relevant courses for logged in person
-    courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
-
-
 @login_required
 def exercises_page(request, course_id):
-    co_exes = CourseExercises.objects.filter(course=course_id)
-    exercises = []
-    for co_ex in co_exes:
-        exercises.append(co_ex.exercise)
-    return render(request, 'exercises.html', {'exercises': exercises})
+    current_user = User(request.user)
+    if current_user.has_perm('student'):
+        course_exercises = CourseExercises.objects.filter(course=course_id)
+        exercises = [ce.exercise for ce in course_exercises]
+        return render(request, 'exercises.html', {'exercises': exercises})
+    elif current_user.has_perm('lecturer'):
+        pass
+    else:
+        pass
+
+
+@login_required
+def courses_page(request):
+    """get's CourseMember matchin the logged in persons pk. Then retrievs the Courses from the CourseMember objects"""
+    current_user = request.user
+    course_member = CourseMembers.objects.filter(student=current_user.id)
+    courses = [cm.course for cm in course_member]
+    return render(request, 'courses.html', {'courses': courses})
