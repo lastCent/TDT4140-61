3c1e9ce977e8e814e968e6e2f356eadec50ee3c8
Andreas Bakke Moan <abakkem@gmail.com>
2017-03-08 15:08:54 +0100

    ALL HAIL PUTIN - THE LEGEND - OUR MASTER

--- frontPage/views.py
+++ frontPage/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.template import loader
+
 # Create your views here.
+
+def index(request):
+    page = loader.get_template('index.html')
+    return HttpResponse(page.render())
--- mainMenu/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.http import HttpResponse
-from django.template import loader
-
-# Create your views here.
-
-def index(request):
-    page = loader.get_template('index.html')
-    return HttpResponse(page.render())
